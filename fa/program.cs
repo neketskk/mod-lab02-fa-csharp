0105a17d13eed810b74e180791646219887190cb
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-04-25 23:02:09 +0300

    fix2

--- fa/program.cs
+++ fa/program.cs
@@ -89,13 +89,13 @@ namespace fans
         public State c = new State()
         {
             Name = "c",
-            IsAcceptState = true,
+            IsAcceptState = false,
             Transitions = new Dictionary<char, State>()
         };
         public State d = new State()
         {
             Name = "d",
-            IsAcceptState = false,
+            IsAcceptState = true,
             Transitions = new Dictionary<char, State>()
         };
         State InitialState = a;
